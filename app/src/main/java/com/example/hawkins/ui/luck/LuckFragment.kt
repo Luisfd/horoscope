6716f0947de12208d57fb3565a38ab32e952dce9
luisfd <luisfd.92@gmail.com>
2023-11-02 03:14:55 +0100

    animations

--- app/src/main/java/com/example/hawkins/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/example/hawkins/ui/luck/LuckFragment.kt
@@ -1,14 +1,23 @@
 package com.example.hawkins.ui.luck
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AlphaAnimation
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.view.animation.DecelerateInterpolator
+import androidx.core.animation.doOnEnd
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import com.example.hawkins.R
 import com.example.hawkins.databinding.FragmentHoroscopeBinding
 import com.example.hawkins.databinding.FragmentLuckBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Random
 
 @AndroidEntryPoint
 class LuckFragment : Fragment() {
@@ -28,6 +37,91 @@ class LuckFragment : Fragment() {
     ): View {
         _binding = FragmentLuckBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+    }
+
+    private fun initUI() {
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.ivRoulette.setOnClickListener{ spinRoulette()}
+    }
+
+    private fun spinRoulette() {
+
+        val random = Random()
+        val degrees = random.nextInt(360*4)+360
+
+        val animator = ObjectAnimator.ofFloat(binding.ivRoulette, View.ROTATION, 0f, degrees.toFloat())
+        animator.duration = 2000
+        animator.interpolator = DecelerateInterpolator() //Animación de rápido a lento
+        animator.doOnEnd{ slideCard() }
+        animator.start()
+    }
+
+    private fun slideCard(){
+        val slideUpAnimation = AnimationUtils.loadAnimation(requireContext(),R.anim.slide_up)
+
+        slideUpAnimation.setAnimationListener(object: Animation.AnimationListener{
+            override fun onAnimationStart(p0: Animation?) {
+                binding.reverse.isVisible = true
+            }
+
+            override fun onAnimationEnd(p0: Animation?) {
+                growCard()
+            }
+
+            override fun onAnimationRepeat(p0: Animation?) {}
+
+        })
+
+        binding.reverse.startAnimation(slideUpAnimation)
+    }
+
+    private fun growCard() {
+        val growAnimation = AnimationUtils.loadAnimation(requireContext(), R.anim.grow)
+        growAnimation.setAnimationListener(object :Animation.AnimationListener{
+            override fun onAnimationStart(p0: Animation?) {}
+
+            override fun onAnimationEnd(p0: Animation?) {
+                binding.reverse.isVisible = false
+                showPremonitionView()
+            }
+
+            override fun onAnimationRepeat(p0: Animation?) {}
+
+        })
+
+        binding.reverse.startAnimation(growAnimation)
+    }
+
+    private fun showPremonitionView() {
+        val dissappearAnimation = AlphaAnimation(1.0f, 0.0f)
+        dissappearAnimation.duration = 200
+
+        val appearAnimation = AlphaAnimation(0.0f, 1.0f)
+        appearAnimation.duration = 1000
+
+        dissappearAnimation.setAnimationListener(object :Animation.AnimationListener{
+            override fun onAnimationStart(p0: Animation?) {}
+
+            override fun onAnimationEnd(p0: Animation?) {
+                binding.preview.isVisible = false
+                binding.prediction.isVisible = true
+            }
+
+            override fun onAnimationRepeat(p0: Animation?) {}
+
+        })
+
+        binding.preview.startAnimation(dissappearAnimation)
+        binding.prediction.startAnimation(appearAnimation)
+
     }
 
 }
